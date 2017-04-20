aa920c2307bd1fa9efd438a94e6f3c6dbf9bb775
jbobball <j.bob.ball@gmail.com>
2017-04-20 10:47:16 -0500

    first check-in

---
+++ Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetmvc.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
